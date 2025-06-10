827015328d7170597a72d9b01e3d3e7f5e448a30
ReMEB <mauguz33@yahoo.com>
2025-06-10 12:43:35 -0600

    Add files via upload

---
+++ res/search.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+/*****************
+	GENERAL SETTINGS
+*****************/
+$imSettings['search']['general'] = array(
+	'menu_position' => 'left',
+	'defaultScope' => array(
+		'0' => 'index.html'
+	),
+	'extendedScope' => array(
+	)
+);
+
+/*****************
+	IMAGES SEARCH
+*****************/
+$imSettings['search']['images'] = array(
+);
+
+/*****************
+	VIDEOS SEARCH
+*****************/
+$imSettings['search']['videos'] = array(
+);
+$imSettings['search']['dynamicobjects'] = array(
+
+);
+
+// End of file search.inc.php
