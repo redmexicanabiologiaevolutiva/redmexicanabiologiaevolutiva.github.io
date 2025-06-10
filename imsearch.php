aa93e67336c24ef5700e4de3dfe1ab96f0f9c9c2
ReMEB <mauguz33@yahoo.com>
2025-06-10 12:41:43 -0600

    Add files via upload

---
+++ imsearch.php
@@ -0,0 +1,153 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="es-ES" dir="ltr">
+	<head>
+		<title>Búsqueda - Primer Congreso Mexicano de Evolución</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="author" content="Altamira Studio" />
+		<meta name="generator" content="Incomedia WebSite X5 Evo 2025.1.2 - www.websitex5.com" />
+		<meta property="og:image" content="http://localhost/favImage_35nvbtas.png" />
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="152">
+		<meta property="og:image:height" content="153">
+		<meta property="og:image" content="http://localhost/favImage_35nvbtas.png" />
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="152">
+		<meta property="og:image:height" content="153">
+		<meta name="twitter:card" content="summary_large_image">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" href="style/reset.css?2025-1-2-1" media="screen,print" />
+		<link rel="stylesheet" href="style/print.css?2025-1-2-1" media="print" />
+		<link rel="stylesheet" href="style/style.css?2025-1-2-1" media="screen,print" />
+		<link rel="stylesheet" href="style/template.css?2025-1-2-1" media="screen" />
+		<link rel="stylesheet" href="pcss/imsearch.css?2025-1-2-1-638851559670271556" media="screen,print" />
+		<script src="res/jquery.js?2025-1-2-1"></script>
+		<script src="res/x5engine.js?2025-1-2-1" data-files-version="2025-1-2-1"></script>
+		<script>
+			window.onload = function(){ checkBrowserCompatibility('El Explorador que estás usando no es compatible con las funciones requeridas para mostrar este Sitio web.','El Navegador que estás utilizando podría no ser compatible con las funciones requeridas para poder ver este Sitio web.','[1]Actualiza tu explorador [/1] o [2]continuar de todos modos[/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'imsearch.php';
+			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
+		</script>
+		<link rel="icon" href="favicon.png?2025-1-2-1-638851559670221701" type="image/png" />
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<a class="screen-reader-only-even-focused" href="#imGoToCont" title="Salta el menu principal">Vaya al Contenido</a>
+				<div id="imHeaderBg"></div>
+				<div id="imPage">
+					<header id="imHeader">
+						<h1 class="imHidden">Búsqueda - Primer Congreso Mexicano de Evolución</h1>
+						<div id="imHeaderObjects"><div id="imHeader_imObjectImage_03_wrapper" class="template-object-wrapper"><div id="imHeader_imObjectImage_03"><div id="imHeader_imObjectImage_03_container"><a href="index.html" onclick="return x5engine.utils.location('index.html', null, false)"><img src="images/01-logo.png"  width="70" height="66" />
+</a></div></div></div><div id="imHeader_imMenuObject_04_wrapper" class="template-object-wrapper"><!-- UNSEARCHABLE --><a id="imHeader_imMenuObject_04_skip_menu" href="#imHeader_imMenuObject_04_after_menu" class="screen-reader-only-even-focused">Saltar menú</a><div id="imHeader_imMenuObject_04"><nav id="imHeader_imMenuObject_04_container"><button type="button" class="clear-button-style hamburger-button hamburger-component" aria-label="Mostrar menú"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></button><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<button type="button" class="clear-button-style hamburger-menu-close-button" aria-label="Cerrar"><span aria-hidden="true">&times;</span></button>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden">
+	<li class="imMnMnFirst imPage" data-link-paths=",/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+INICIO		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imLevel" data-link-paths=",/index.html,/" data-link-anchor="temas" data-link-hash="-595072728"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#temas" class="label" onclick="return x5engine.utils.location('index.html#temas', null, false)">TEMÁTICAS</a></div></div></li><li class="imMnMnMiddle imLevel" data-link-paths=",/index.html,/" data-link-anchor="speakers" data-link-hash="-1295803864"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#speakers" class="label" onclick="return x5engine.utils.location('index.html#speakers', null, false)">SPEAKERS</a></div></div></li><li class="imMnMnMiddle imLevel" data-link-paths=",/index.html,/" data-link-anchor="preregistro" data-link-hash="64905306"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#preregistro" class="label" onclick="return x5engine.utils.location('index.html#preregistro', null, false)">PRE-REGISTRO</a></div></div></li><li class="imMnMnMiddle imLevel" data-link-paths=",/index.html,/" data-link-anchor="talleres" data-link-hash="-952415954"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#talleres" class="label" onclick="return x5engine.utils.location('index.html#talleres', null, false)">TALLERES</a></div></div></li><li class="imMnMnMiddle imLevel" data-link-paths=",/index.html,/" data-link-anchor="inscripcion" data-link-hash="-379724425"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#inscripcion" class="label" onclick="return x5engine.utils.location('index.html#inscripcion', null, false)">COSTOS</a></div></div></li><li class="imMnMnMiddle imLevel" data-link-paths=",/index.html,/" data-link-anchor="costos" data-link-hash="291843863"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#costos" class="label" onclick="return x5engine.utils.location('index.html#costos', null, false)">MEMBRESÍAS</a></div></div></li><li class="imMnMnLast imLevel" data-link-paths=",/index.html,/" data-link-anchor="contacto" data-link-hash="-1171706541"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#contacto" class="label" onclick="return x5engine.utils.location('index.html#contacto', null, false)">CONTACTO</a></div></div></li></ul></nav></div><a id="imHeader_imMenuObject_04_after_menu" class="screen-reader-only-even-focused"></a><!-- UNSEARCHABLE END --><script>
+var imHeader_imMenuObject_04_settings = {
+	'menuId': 'imHeader_imMenuObject_04',
+	'responsiveMenuEffect': 'slide',
+	'responsiveMenuLevelOpenEvent': 'click',
+	'animationDuration': 1000,
+}
+x5engine.boot.push(function(){x5engine.initMenu(imHeader_imMenuObject_04_settings)});
+$(function () {
+    $('#imHeader_imMenuObject_04_container ul li').not('.imMnMnSeparator').each(function () {
+        $(this).on('mouseenter', function (evt) {
+            if (!evt.originalEvent) {
+                evt.stopImmediatePropagation();
+                evt.preventDefault();
+                return;
+            }
+        });
+    });
+});
+$(function () {$('#imHeader_imMenuObject_04_container ul li').not('.imMnMnSeparator').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imHeader_imMenuObject_04_container-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imHeader_imMenuObject_04_container-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+$(function () {$('#imHeader_imMenuObject_04_container > ul > li').not('.imMnMnSeparator').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        clearTimeout(timeout);        var overElem = $this.children('.sweeper');        if(overElem.length == 0)            overElem = $this.children('.label-wrapper').clone().appendTo($this).wrap('<div class="sweeper"></div>').addClass('label-wrapper-over').parent();        setTimeout(function(){overElem.addClass('animated');}, 10);    }).on('mouseleave', function () {        var overElem = $this.children('.sweeper');        overElem.removeClass('animated');        timeout = setTimeout(function(){overElem.remove();}, 500);    });});});
+</script>
+</div></div>
+					</header>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"><div id="imStickyBar_imObjectImage_01_wrapper" class="template-object-wrapper"><div id="imStickyBar_imObjectImage_01"><div id="imStickyBar_imObjectImage_01_container"><img src="images/01-ico.png"  width="58" height="61" />
+</div></div></div><div id="imStickyBar_imMenuObject_02_wrapper" class="template-object-wrapper"><!-- UNSEARCHABLE --><a id="imStickyBar_imMenuObject_02_skip_menu" href="#imStickyBar_imMenuObject_02_after_menu" class="screen-reader-only-even-focused">Saltar menú</a><div id="imStickyBar_imMenuObject_02"><nav id="imStickyBar_imMenuObject_02_container"><button type="button" class="clear-button-style hamburger-button hamburger-component" aria-label="Mostrar menú"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></button><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<button type="button" class="clear-button-style hamburger-menu-close-button" aria-label="Cerrar"><span aria-hidden="true">&times;</span></button>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden">
+	<li class="imMnMnFirst imPage" data-link-paths=",/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+INICIO		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imLevel" data-link-paths=",/index.html,/" data-link-anchor="temas" data-link-hash="-595072728"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#temas" class="label" onclick="return x5engine.utils.location('index.html#temas', null, false)">TEMÁTICAS</a></div></div></li><li class="imMnMnMiddle imLevel" data-link-paths=",/index.html,/" data-link-anchor="speakers" data-link-hash="-1295803864"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#speakers" class="label" onclick="return x5engine.utils.location('index.html#speakers', null, false)">SPEAKERS</a></div></div></li><li class="imMnMnMiddle imLevel" data-link-paths=",/index.html,/" data-link-anchor="preregistro" data-link-hash="64905306"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#preregistro" class="label" onclick="return x5engine.utils.location('index.html#preregistro', null, false)">PRE-REGISTRO</a></div></div></li><li class="imMnMnMiddle imLevel" data-link-paths=",/index.html,/" data-link-anchor="talleres" data-link-hash="-952415954"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#talleres" class="label" onclick="return x5engine.utils.location('index.html#talleres', null, false)">TALLERES</a></div></div></li><li class="imMnMnMiddle imLevel" data-link-paths=",/index.html,/" data-link-anchor="inscripcion" data-link-hash="-379724425"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#inscripcion" class="label" onclick="return x5engine.utils.location('index.html#inscripcion', null, false)">COSTOS</a></div></div></li><li class="imMnMnMiddle imLevel" data-link-paths=",/index.html,/" data-link-anchor="costos" data-link-hash="291843863"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#costos" class="label" onclick="return x5engine.utils.location('index.html#costos', null, false)">MEMBRESÍAS</a></div></div></li><li class="imMnMnLast imLevel" data-link-paths=",/index.html,/" data-link-anchor="contacto" data-link-hash="-1171706541"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#contacto" class="label" onclick="return x5engine.utils.location('index.html#contacto', null, false)">CONTACTO</a></div></div></li></ul></nav></div><a id="imStickyBar_imMenuObject_02_after_menu" class="screen-reader-only-even-focused"></a><!-- UNSEARCHABLE END --><script>
+var imStickyBar_imMenuObject_02_settings = {
+	'menuId': 'imStickyBar_imMenuObject_02',
+	'responsiveMenuEffect': 'slide',
+	'responsiveMenuLevelOpenEvent': 'click',
+	'animationDuration': 1000,
+}
+x5engine.boot.push(function(){x5engine.initMenu(imStickyBar_imMenuObject_02_settings)});
+$(function () {
+    $('#imStickyBar_imMenuObject_02_container ul li').not('.imMnMnSeparator').each(function () {
+        $(this).on('mouseenter', function (evt) {
+            if (!evt.originalEvent) {
+                evt.stopImmediatePropagation();
+                evt.preventDefault();
+                return;
+            }
+        });
+    });
+});
+$(function () {$('#imStickyBar_imMenuObject_02_container ul li').not('.imMnMnSeparator').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imStickyBar_imMenuObject_02_container-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imStickyBar_imMenuObject_02_container-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+$(function () {$('#imStickyBar_imMenuObject_02_container > ul > li').not('.imMnMnSeparator').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        clearTimeout(timeout);        var overElem = $this.children('.sweeper');        if(overElem.length == 0)            overElem = $this.children('.label-wrapper').clone().appendTo($this).wrap('<div class="sweeper"></div>').addClass('label-wrapper-over').parent();        setTimeout(function(){overElem.addClass('animated');}, 10);    }).on('mouseleave', function () {        var overElem = $this.children('.sweeper');        overElem.removeClass('animated');        timeout = setTimeout(function(){overElem.remove();}, 500);    });});});
+</script>
+</div></div>
+						</div>
+					</div>
+					<div id="imSideBar">
+						<div id="imSideBarObjects"></div>
+					</div>
+					<div id="imContentGraphics"></div>
+					<main id="imContent">
+						<a id="imGoToCont"></a>
+						<div id="imSearchPage">
+						<h2 id="imPgTitle">Resultados de búsqueda</h2>
+						<?php
+						$search = new imSearch();
+						$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search'], ENT_COMPAT) : "";
+						$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+						$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+						<div class="searchPageContainer">
+						<?php echo $search->search($keys, $page, $type); ?>
+						</div>
+						</div>
+						
+					</main>
+					<div id="imFooterBg"></div>
+					<footer id="imFooter">
+						<div id="imFooterObjects"></div>
+					</footer>
+				</div>
+				<span class="screen-reader-only-even-focused"><a href="#imGoToCont" title="Lea esta página de nuevo">Regreso al contenido</a></span>
+			</div>
+		</div>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Para utilizar este sitio tienes que habilitar JavaScript.</div></noscript>
+	</body>
+</html>
