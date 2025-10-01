136a7779955f93f3cf9d2beec64ddef25008f1c0
ReMEB <mauguz33@yahoo.com>
2025-10-01 13:59:17 -0600

    Add files via upload

--- res/x5settings.php
+++ res/x5settings.php
@@ -53,8 +53,8 @@ $imSettings['password_policy'] = array(
 |	Captcha
 |-------------------------------
 */ImTopic::$captcha_code = "		<div class=\"x5captcha-wrap\">
-			<label for=\"qbvh2twc-imCpt\">Palabra de control:</label><br />
-			<input type=\"text\" id=\"qbvh2twc-imCpt\" class=\"imCpt\" name=\"imCpt\" maxlength=\"5\" />
+			<label for=\"io77olib-imCpt\">Palabra de control:</label><br />
+			<input type=\"text\" id=\"io77olib-imCpt\" class=\"imCpt\" name=\"imCpt\" maxlength=\"5\" />
 		</div>
 ";
 
