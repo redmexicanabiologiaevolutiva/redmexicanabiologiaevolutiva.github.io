489ba4d57df66b7cc2785bcafdb2f680a2c6ec97
ReMEB <mauguz33@yahoo.com>
2025-11-14 20:03:47 -0600

    Add files via upload

--- captcha/x5captcha.php
+++ captcha/x5captcha.php
@@ -1,7 +1,7 @@
 <?php
 include("../res/x5engine.php");
-$nameList = array("vnf","whx","pf2","rnh","ck5","zh8","7e8","uuj","nhm","xf6");
-$charList = array("H","J","3","4","C","J","A","Y","V","L");
+$nameList = array("a5u","lxp","rn6","482","3yu","xv8","28t","ryz","833","wk7");
+$charList = array("S","E","L","E","N","Z","C","E","3","4");
 $cpt = new X5Captcha($nameList, $charList);
 //Check Captcha
 if ($_GET["action"] == "check")
